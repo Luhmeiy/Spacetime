741cc8e7d9686ce9001dccbb3a20445bf1022915
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-05-18 15:33:35 -0300

    Add authentication for user

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import fastify from "fastify";
+import jwt from "@fastify/jwt";
 import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 
@@ -8,10 +11,16 @@ app.register(cors, {
 	origin: true,
 });
 
+app.register(jwt, {
+	secret: "spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.listen({
 	port: 3333,
+	host: "0.0.0.0",
 }).then(() => {
 	console.log("🚀 HTTP server running on http://localhost:3333");
 });
