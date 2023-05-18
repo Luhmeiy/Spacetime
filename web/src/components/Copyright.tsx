de8cdd255e718f82ad5ea911c125864b4c35735c
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-05-18 14:35:51 -0300

    Add login/register with GitHub

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export default function Copyright() {
+	return (
+		<div className="text-sm leading-relaxed text-gray-200">
+			Feito com 💜 no NLW da{" "}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.rocketseat.com.br/"
+				className="underline hover:text-gray-100
+                "
+			>
+				Rocketseat
+			</a>
+		</div>
+	);
+}
---
+++ web/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Copyright } from "./Copyright";
+export { default as Hero } from "./Hero";
+export { default as Profile } from "./Profile";
+export { default as SignIn } from "./SignIn";
+export { default as EmptyMemories } from "./EmptyMemories";
