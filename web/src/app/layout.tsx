cae87e4f113ef4cf73f0d4d7ea0588f6d95fe7e6
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-05-19 01:14:01 -0300

    Add page to create new memories

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,8 @@ import {
 	Roboto_Flex as Roboto,
 	Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google";
+import { Copyright, Hero, Profile, SignIn } from "@/components";
+import { cookies } from "next/headers";
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 const baiJamjuree = BaiJamjuree({
@@ -19,12 +21,31 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+	const isAuthenticated = cookies().has("token");
+
 	return (
 		<html lang="en">
 			<body
 				className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
 			>
-				{children}
+				<main className="grid min-h-screen grid-cols-2">
+					<div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+						{/* Blur */}
+						<div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+						{/* Stripes */}
+						<div className="right-o absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+
+						{isAuthenticated ? <Profile /> : <SignIn />}
+
+						<Hero />
+						<Copyright />
+					</div>
+
+					<div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+						{children}
+					</div>
+				</main>
 			</body>
 		</html>
 	);
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,28 +1,5 @@
-import { cookies } from "next/headers";
-
-import { Copyright, EmptyMemories, Hero, Profile, SignIn } from "@/components";
+import { EmptyMemories } from "@/components";
 
 export default function Home() {
-	const isAuthenticated = cookies().has("token");
-
-	return (
-		<main className="grid min-h-screen grid-cols-2">
-			<div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-				{/* Blur */}
-				<div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
-
-				{/* Stripes */}
-				<div className="right-o absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
-
-				{isAuthenticated ? <Profile /> : <SignIn />}
-
-				<Hero />
-				<Copyright />
-			</div>
-
-			<div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-				<EmptyMemories />
-			</div>
-		</main>
-	);
+	return <EmptyMemories />;
 }
