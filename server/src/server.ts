c8c439e3abaa105ad0e799d12d54a0de1bdfcc85
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-05-19 01:16:22 -0300

    Add file upload route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
 import "dotenv/config";
 import fastify from "fastify";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+	root: resolve(__dirname, "../uploads"),
+	prefix: "/uploads",
+});
 
 app.register(cors, {
 	origin: true,
@@ -16,6 +26,7 @@ app.register(jwt, {
 });
 
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 app.listen({
